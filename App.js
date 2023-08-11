b09e7fc4e13cbc8a2027b42784040b23aee98fc8
gudofabi <godofredocpanel@gmail.com>
2023-08-11 23:56:39 +0800

    fix: start layout

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      backgroundColor: "white",
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: 'center',
+      alignContent: "center",
+      flexWrap: "wrap"
+    }}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "greenyellow",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "grey",
+        width: 100,
+        height: 100
+      }}/>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
